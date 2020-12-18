c39c95ffa12a1cee171985dd5ae684e02b68330b
Felipe <bd.bumdigital@gmail.com>
2020-12-18 10:20:01 -0300

    pedido enviado

--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -9,25 +9,28 @@ class Formulario extends Component{
       <Form method="post" action="http://localhost/Cavaleiro-Jedi/public/api/pedido.php">
         <Form.Row className="d-flex justify-content-center">
           <Col className="py-1" xs={12} sm={6} md={4} lg={2}>
-            <Form.Control placeholder="Cliente" name="nome" />
+            <Form.Control placeholder="Cliente" name="nome_do_cliente" />
           </Col>
           <Col className="py-1" xs={12} sm={6} md={4} lg={2}>
-            <Form.Control placeholder="Endereço" name="endereço" />
+            <Form.Control placeholder="Endereço" name="endereco" />
           </Col>
           <Col className="py-1" xs={12} sm={6} md={4} lg={2}>
             <Form.Control placeholder="Telefone" name="telefone" />
           </Col>
           <Col className="py-1" xs={12} sm={6} md={4} lg={2}>
-            <Form.Control as="select" name="produto">
+            <Form.Control as="select" name="nome_do_produto">
               <option value="0">Produto</option>
               {this.props.data.map((produto, index) => <option key={index} value={produto.id}>{produto.nome}</option>)}
           </Form.Control>
         </Col>
           <Col className="py-1" xs={12} sm={6} md={4} lg={1}>
+            <Form.Control placeholder="Valor" name="valor_unitario" />
+          </Col>
+          <Col className="py-1" xs={12} sm={6} md={4} lg={1}>
             <Form.Control placeholder="Quantidade" name="quantidade" />
           </Col>
           <Col className="py-1" xs={12} sm={6} md={4} lg={1}>
-            <Form.Control placeholder="Total" name="total" />
+            <Form.Control placeholder="Total" name="valor_total" />
           </Col>
           <Button xs={12} sm={6} md={4} lg={2} variant="success" type="submit">Enviar</Button>
       </Form.Row>
